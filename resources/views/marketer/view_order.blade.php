69aebbc6a9723ceec65618dd755b01db1e5e9ec1
OLUOKUN KABIRU ADESINA <oluokunkabiru2015@gmail.com>
2020-08-30 13:17:19 +0100

    add orders view to marketers

--- resources/views/marketer/view_order.blade.php
+++ resources/views/marketer/view_order.blade.php
@@ -8,56 +8,79 @@
                 <img src="{{ $view->picture->file }}" alt="{{ $view->product_name }}" class="card-img">
             </div>
         </div>
-        <div class="col-lg-4 col-md-4">
+
+        <div class="col-lg-7 col-md-7">
             <!-- small box -->
-            <div class="small-box bg-info">
-            <div class="inner">
-                <h3> {{ $view->product_name }} </h3>
-                <p>Product Name</p>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3> {{ucwords( $view->product_name) }} </h3>
+                        <p>Product Name</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3> {{ $view->quantity }} </h3>
+                        <p>Quantity</p>
+                    </div>
+                    </div>
+                </div>
             </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3> {{ $view->billing_price }} </h3>
+                        <p>Price</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3> {{ $view->billing_total_price }} </h3>
+                        <p>Total Price</p>
+                    </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="col-lg-3 col-md-3">
-            <div class="small-box bg-info">
-            <div class="inner">
-                <h3>  {{ $view->quantity }}</h3>
-                <p>Order Quantity</p>
-            </div>
-            </div>
+
         </div>
 
     </div>
 
+        <h3 class="text-center">Customers Details</h3>
 
     <div class="row">
-        <h3>Customers Address Details</h3>
-        <div class="col-lg-3 col-md-3">
+        <div class="col-lg-6 col-md-6 col-sm-6">
             <!-- small box -->
-            <div class="small-box bg-warning">
+            <div class="small-box bg-dark">
             <div class="inner">
-                <h3 > {{ $view->billing_country }} </h3>
-                <p>Country</p>
+                <h5> Country :: {{ucwords($view->billing_country) }} </h5>
+                <h5> State :: {{ ucwords($view->billing_state) }} </h5>
+                <h5> City ::{{ ucwords($view->billing_city) }} </h5>
+                <h5> Address ::{{ ucwords($view->billing_address) }} </h5>
+                <h5> Zipcode ::{{ $view->billing_zipcode }} </h5>
+
             </div>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3">
+        <div class="col-lg-6 col-md-6 col-sm-6">
             <!-- small box -->
-            <div class="small-box bg-success">
+            <div class="small-box bg-dark">
             <div class="inner">
-                <h3 >{{ $view->newprice }}</h3>
-                <p>Product New Price</p>
+                <h4> Phone No :: {{ $view->billing_phone }} </h4>
+                <h4> Email :: {{ $view->billing_email }} </h4>
+                <h4> Payment Method :: {{ucwords( $view->billing_payment_method) }} </h4>
+
+
             </div>
             </div>
         </div>
-        <div class="col-lg-4 col-md-4">
-            <!-- small box -->
-            <div class="small-box bg-secondary">
-            <div class="inner">
-                <h3 >{{ $view->quantity }} </h3>
-                <p>Available Quantity</p>
-            </div>
-            </div>
-        </div>
+
     </div>
 
 @endsection
